f5b49a5975c269f34e5da26cccfe83bf4d3cd459
Mike <mihail.shch@gmail.com>
2019-06-06 21:07:54 +0300

    First working module test

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
+from django.urls import resolve
+from .views import home_page
 
-class SmokeTest(TestCase):
-    def test_math_bad(self):
-        self.assertEqual(1+1, 3)
+class HomePageTest(TestCase):
+    def test_home_page(self):
+        found=resolve('/')
+        self.assertEqual(found.func, home_page)
