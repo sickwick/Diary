8da21487e3a074023d299a1b4410926e9de9f8bf
Mike <mihail.shch@gmail.com>
2019-06-06 20:15:39 +0300

    Expended FT with use of unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,25 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert "Django" in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        # The announcement the browser
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        # Close browser
+        self.browser.quit()
+
+    def test_can_start_list_and_retrieve_it_later(self):
+        # The announcement of page address
+        self.browser.get('http://localhost:8000')
+
+        # Search for "Diary" word in the title
+        self.assertIn("Diary", self.browser.title)
+        self.fail("Conclude the test")
+
+
+if __name__ == '__main__':
+    unittest.main()
