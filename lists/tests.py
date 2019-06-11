eeb03691b7c7368181f92a8f97fc3d9aeebd0c94
Mike <mihail.shch@gmail.com>
2019-06-11 14:53:34 +0300

    Add first model with text field

--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,7 @@
 from django.test import TestCase
-from django.urls import resolve
-from django.http import HttpRequest
-from django.template.loader import render_to_string
 
 from .views import home_page
+from .models import Item
 
 
 class HomePageTest(TestCase):
@@ -17,10 +15,20 @@ class HomePageTest(TestCase):
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'lists/home.html')
 
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item=Item()
+        first_item.text = "The first item ever"
+        first_item.save()
 
+        second_item=Item()
+        second_item.text = "Second item"
+        second_item.save()
 
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
 
-
-
-
-
+        first_saved_item=saved_items[0]
+        second_item_saved = saved_items[1]
+        self.assertEqual(first_saved_item.text, "The first item ever")
+        self.assertEqual(second_item_saved.text, "Second item")
