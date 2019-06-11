988840cf3ef0028fb1c53c0370cb13a87513911f
Mike <mihail.shch@gmail.com>
2019-06-11 19:21:33 +0300

    Redirect POST and list of all items in db

--- lists/tests.py
+++ lists/tests.py
@@ -12,8 +12,21 @@ class HomePageTest(TestCase):
 
     def test_can_save_a_post_request(self):
         response = self.client.post("/", data = {'item_text': 'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'lists/home.html')
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post("/", data = {'item_text': 'A new list item'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+
+    def test_save_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
 
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
@@ -32,3 +45,12 @@ class ItemModelTest(TestCase):
         second_item_saved = saved_items[1]
         self.assertEqual(first_saved_item.text, "The first item ever")
         self.assertEqual(second_item_saved.text, "Second item")
+
+    def test_display_all_list_items(self):
+        Item.objects.create(text = 'item1')
+        Item.objects.create(text = 'item2')
+
+        responce = self.client.get('/')
+
+        self.assertIn('item1', responce.content.decode())
+        self.assertIn('item2', responce.content.decode())
